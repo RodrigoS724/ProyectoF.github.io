ba7671d4cc43abd5b95cc3b3708ef141e2770f7a
RodrigoS724 <rodrigosayas24@gmail.com>
2023-09-13 00:16:00 -0300

    Script andando al fin

---
+++ scripts/BDconnection/BDconnection.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mysql from "mysql";
+const port = "3000";
+const app = express();
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    database:'db_multisales',
+    user:'root',
+    password:'';
+});
+
+connection.connect(function(error){
+    if(error){
+        console.log('error en conexion db', error.stack);
+        return;
+    }
+    console.log('conectado', connection.threadId);
+});
+
+app.
--- scripts/cargarArticulos.js
+++ scripts/cargarArticulos.js
@@ -4,16 +4,16 @@ function addCompany() {
     let nameCom = "JoyeriaRoy";
     let description = "Holaaa";
     let addres = "ny";
-    let divHtml ='<div class="cards__card card">' + '<h2 class="card__heading" id="nameCompany">' + nameCom + '</h2>' +
-    '<ul role="list" class="card__bullets flow">' + '<li id="descriptionCompany">' + description + '</li>' +
+    let divHtml ='<div class="cards__card card"><h2 class="card__heading" id="nameCompany">' + nameCom + '</h2>' +
+    '<ul role="list" class="card__bullets flow"><li id="descriptionCompany">' + description + '</li>' +
    '<li id="addressCompany">' + addres + '</li></ul><a href="#basic" class="card__cta cta">Ver</a></div>';
     return divHtml;
 }
 (function ejecutar(){
     const list = document.getElementById("cardCompany");
     let newCard = addCompany();
-    for (let index = 0; index < array.length; index++) {
-        list.insertAdjacentHTML("beforebegin", newCard);
+    for (let index = 0; index < 4; index++) {
+        list.insertAdjacentHTML("afterbegin", newCard);
     }
 })();
 
