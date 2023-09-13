78d8d61cb964f2a0346de0d51d61a0957859d478
RodrigoS724 <rodrigosayas24@gmail.com>
2023-09-12 22:43:34 -0300

    new comit

---
+++ scripts/cargarArticulos.js
@@ -0,0 +1,19 @@
+
+
+function addCompany() {
+    let nameCom = "JoyeriaRoy";
+    let description = "Holaaa";
+    let addres = "ny";
+    let divHtml ='<div class="cards__card card">' + '<h2 class="card__heading" id="nameCompany">' + nameCom + '</h2>' +
+    '<ul role="list" class="card__bullets flow">' + '<li id="descriptionCompany">' + description + '</li>' +
+   '<li id="addressCompany">' + addres + '</li></ul><a href="#basic" class="card__cta cta">Ver</a></div>';
+    return divHtml;
+}
+(function ejecutar(){
+    const list = document.getElementById("cardCompany");
+    let newCard = addCompany();
+    for (let index = 0; index < array.length; index++) {
+        list.insertAdjacentHTML("beforebegin", newCard);
+    }
+})();
+
