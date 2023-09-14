3866afba80a73df6dd442895ac10a6b038792b36
RodrigoS724 <rodrigosayas24@gmail.com>
2023-09-14 15:04:19 -0300

    Update cargarArticulos.js

--- scripts/cargarArticulos.js
+++ scripts/cargarArticulos.js
@@ -1,18 +1,20 @@
+var nameCom = ['JoyeriaRoy', 'Aliexpress', 'Musa', 'Peluqueria el rocho'];
+var description = ['Joyas de todo tipo', 'baratito', 'pasteleria', 'cortes bien rochos'];
+var addres = ['paysandu, Uruguay', 'China', 'Mercedes, Uruguay', 'paysandu, Uruguay'];
 
-
-function addCompany() {
-    let nameCom = "JoyeriaRoy";
-    let description = "Holaaa";
-    let addres = "ny";
-    let divHtml ='<div class="cards__card card"><h2 class="card__heading" id="nameCompany">' + nameCom + '</h2>' +
-    '<ul role="list" class="card__bullets flow"><li id="descriptionCompany">' + description + '</li>' +
-   '<li id="addressCompany">' + addres + '</li></ul><a href="#basic" class="card__cta cta">Ver</a></div>';
+function addCompany( _name, _description, _addres) {
+    let nameCom = _name;
+    let description = _description;
+    let addres = _addres;
+    let divHtml = '<div class="cards__card card"><h2 class="card__heading" id="nameCompany">' + nameCom + '</h2>' +
+        '<ul role="list" class="card__bullets flow"><li id="descriptionCompany">' + description + '</li>' +
+        '<li id="addressCompany">' + addres + '</li></ul><a href="#basic" class="card__cta cta">Ver</a></div>';
     return divHtml;
 }
-(function ejecutar(){
+(function ejecutar() {
     const list = document.getElementById("cardCompany");
-    let newCard = addCompany();
     for (let index = 0; index < 4; index++) {
+        let newCard = addCompany(nameCom[index], description[index], addres[index]);
         list.insertAdjacentHTML("afterbegin", newCard);
     }
 })();
