b0b9450f1b100cf33a92416944a5a38ec7ffc54a
RodrigoS724 <rodrigosayas24@gmail.com>
2024-02-12 21:14:30 -0300

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,10 @@
 let textoEnHTML = [
-    "Hola Flavii!!!        Bienvenida",
-    "Esto es un pequeño regalo que se atrazo unos días, dado al poco tiempo que he tenido",
-    "Simplemente queria decirte lo mucho que te amoo y ...",
-    "el gran aprecio que te tengo de una forma distinta para variar un poco :)",
-    "y tambien ya de paso decirte que ..",
-    "..",
-    "..",
-    "Nada jajaja me gusta mucho molestarte",
-    "por ende te pido discuplas por los calentones de cabeza..",
-    "que te he echo dar jajaja",
-    "TE AMOO MOROCHA LINDAA",
-    "FIIIIIINNNN"
+    "Esta es la version 2.0 San Valentin",
+    "Feliz San Valentin Morochitaaaa",
+    "Me haces re feliz preciosa",
+    "Se que esto no es la gran cosa pero te lo dedico para vos",
+    "Que sos lo mejor que me ha pasado",
+    "TE AMOOOO"
 ]
 
 function nextText() {
@@ -22,7 +16,7 @@ function nextText() {
         console.log(textoCard.textContent === textoEnHTML[i])
         if (textoCard.textContent === textoEnHTML[i]) {
             console.log("entre")
-            if (i + 1 < 12) {
+            if (i + 1 < 6) {
                 position = i
             }else{
                 position = -1
